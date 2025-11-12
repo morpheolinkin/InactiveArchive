e8b948ae62b111c9fabdcebd5352d051fa09b4a8
Jefferson <jeffesonmedeirosdasilva@gmail.com>
2025-11-12 15:27:27 -0300

    feat: Configura perfis de banco (dev/prod)

---
+++ src/main/java/com/escola/inactivearchive/InactiveArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.escola.inactivearchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InactiveArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InactiveArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escola/inactivearchive/InactiveArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escola.inactivearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InactiveArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
