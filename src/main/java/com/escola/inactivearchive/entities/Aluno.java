fb6c5d1de45f0af4ee4041670a8db7f71c7a29f7
Jefferson <jeffesonmedeirosdasilva@gmail.com>
2025-11-12 15:51:03 -0300

    feat: Adiciona entidade Aluno e repositório correspondente

---
+++ src/main/java/com/escola/inactivearchive/entities/Aluno.java
@@ -0,0 +1,25 @@
+package com.escola.inactivearchive.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "aluno")
+@Entity
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true)
+    private String cpf;
+    @Column(name = "data_nascimento",nullable = false)
+    private LocalDate dataNascimento;
+}
---
+++ src/main/java/com/escola/inactivearchive/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.escola.inactivearchive.repository;
+
+import com.escola.inactivearchive.entities.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
